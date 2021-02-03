2c729716ce7e21df2fb432cb296edfd52f5e64d0
Marcos <52284432+tore2003@users.noreply.github.com>
2021-02-03 10:09:48 +0100

    Delete ot directory

--- ot/scrips/disable_panorama_blur.js
+++
@@ -1,3 +0,0 @@
-UI.SetValue( "Misc", "GENERAL", "Misc", "Force sv_cheats", true );
-UI.SetValue( "Misc", "GENERAL", "Misc", "Hidden cvars", true );
-Global.ExecuteCommand( "@panorama_disable_blur 1" );
